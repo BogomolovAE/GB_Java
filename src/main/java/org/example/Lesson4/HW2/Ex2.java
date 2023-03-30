18a65f15d9de762406bf013e6438feaa2feb506d
Alexander Bogomolov <a.e.bogomolov@gmail.com>
2023-03-30 18:18:55 +0300

    Lesson4 HW Done

--- src/main/java/org/example/Lesson4/HW2/Ex2.java
+++ src/main/java/org/example/Lesson4/HW2/Ex2.java
@@ -1,4 +1,30 @@
 package org.example.Lesson4.HW2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 public class Ex2 {
+    public static void main(String[] args) {
+
+        String s = "asdfasdfafgsdfgh[]f(sadfas[sdf])sdf{sdfsdfghdfgh{sdfgdfgh}sdghsdfgh}";
+        System.out.println(StringValidator(s));
+        }
+    public static boolean StringValidator (String s){
+        ArrayList<Character> openingBrackets = new ArrayList<>(Arrays.asList('[','{','('));
+        ArrayList<Character> closingBrackets = new ArrayList<>(Arrays.asList(']','}',')'));
+        LinkedList<Character> sequenceOfBrackets=new LinkedList<>();
+        for (int i=0;i<s.length();i++){
+            if (openingBrackets.contains(s.charAt(i))){
+                sequenceOfBrackets.addLast(s.charAt(i));
+            } else if (closingBrackets.contains(s.charAt(i))) {
+                if (sequenceOfBrackets.size()==0) return false;
+                if (openingBrackets.indexOf(sequenceOfBrackets.getLast())==closingBrackets.indexOf(s.charAt(i))){
+                    sequenceOfBrackets.removeLast();
+                }
+                else return false;
+            }
+    }
+        if (sequenceOfBrackets.size()==0) return true;
+        else return false;
+}
 }
